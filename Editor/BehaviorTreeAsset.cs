a1548102371f3dd830e1d9a6033b80e0ff3bd92c
WarperSan <leumas.ecole@gmail.com>
2025-09-08 17:34:24 -0400

    Created basic BT asset

---
+++ Editor/BehaviorTreeAsset.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace TreeFlow.Editor
+{
+    /// <summary>
+    /// Asset that represents a behavior tree usable by the editor
+    /// </summary>
+    [CreateAssetMenu(menuName = "TreeFlow/Behavior Tree")]
+    public class BehaviorTreeAsset : ScriptableObject
+    {
+        
+    }
+}
