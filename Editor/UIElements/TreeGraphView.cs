21b8deea357b35ea4b525164dbc89ad83d9fbafe
WarperSan <leumas.ecole@gmail.com>
2025-09-08 18:51:16 -0400

    Added basic graph view

---
+++ Editor/UIElements/TreeGraphView.cs
@@ -0,0 +1,38 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace TreeFlow.Editor.UIElements
+{
+    /// <summary>
+    /// Element used to display and modify <see cref="BehaviorTreeAsset"/>
+    /// </summary>
+    internal class TreeGraphView : GraphView
+    {
+        public TreeGraphView()
+        {
+            CreateUI();
+            
+            this.AddManipulator(new ContentDragger());
+            this.AddManipulator(new ContentZoomer());
+            this.AddManipulator(new SelectionDropper());
+            this.AddManipulator(new RectangleSelector());
+        }
+
+        #region UI
+
+        public new class UxmlFactory : UxmlFactory<TreeGraphView, UxmlTraits> { }
+
+        private void CreateUI()
+        {
+            styleSheets.Add(Helpers.Resources.Load<StyleSheet>("StyleSheets/TreeGraphView.uss"));
+            
+            var background = new GridBackground();
+            background.AddToClassList("grid");
+            background.name = "Background";
+            Add(background);
+        }
+
+        #endregion
+    }
+}
