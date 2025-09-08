8bcd86200ca636fe674ef52961335eaa49edfdf7
WarperSan <leumas.ecole@gmail.com>
2025-09-08 17:34:38 -0400

    Created basic BT editor window

---
+++ Editor/TreeFlowEditorWindow.cs
@@ -0,0 +1,12 @@
+using UnityEditor;
+
+namespace TreeFlow.Editor
+{
+    /// <summary>
+    /// Editor window that allows to see and design <see cref="BehaviorTreeAsset"/>
+    /// </summary>
+    public class TreeFlowEditorWindow : EditorWindow
+    {
+        
+    }
+}
