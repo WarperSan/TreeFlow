41649409ec5cadc4366c9065de0f4ec0979a7ea1
WarperSan <leumas.ecole@gmail.com>
2025-09-08 18:36:07 -0400

    Added doc

--- Editor/UIElements/SplitView.cs
+++ Editor/UIElements/SplitView.cs
@@ -2,6 +2,9 @@ using UnityEngine.UIElements;
 
 namespace TreeFlow.Editor.UIElements
 {
+    /// <summary>
+    /// Element used to display a view split into two panes 
+    /// </summary>
     internal class SplitView : TwoPaneSplitView
     {
         public new class UxmlFactory : UxmlFactory<SplitView, UxmlTraits> { }
