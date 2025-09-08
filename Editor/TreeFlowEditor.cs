22946c94ad46a3d024a7eddd164be3f4cc2a20a6
WarperSan <leumas.ecole@gmail.com>
2025-09-08 17:34:52 -0400

    Added basic BT asset's inspector

---
+++ Editor/TreeFlowEditor.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace TreeFlow.Editor
+{
+    /// <summary>
+    /// Editor that handles the link with the base client
+    /// </summary>
+    [CustomEditor(typeof(BehaviorTreeAsset))]
+    public class TreeFlowEditor : UnityEditor.Editor
+    {
+        /// <summary>
+        /// Opens <see cref="TreeFlowEditorWindow"/> with the given asset
+        /// </summary>
+        private static void OpenWindow(BehaviorTreeAsset asset)
+        {
+            var window = EditorWindow.GetWindow<TreeFlowEditorWindow>();
+            window.titleContent.text = "Behavior Tree";
+            window.ShowTab();
+        }
+        
+        [MenuItem("Window/TreeFlow/Designer")]
+        private static void ShowWindow() => OpenWindow(null);
+        
+        /// <inheritdoc/>
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Open"))
+                OpenWindow(target as BehaviorTreeAsset);
+            
+            //base.OnInspectorGUI();
+        }
+    }
+}
