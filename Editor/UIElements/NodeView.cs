a2efe26e6b7814ab7011a8e06ab7ed4d7e7b2974
WarperSan <leumas.ecole@gmail.com>
2025-09-08 20:28:51 -0400

    Added move + remove nodes

--- Editor/UIElements/NodeView.cs
+++ Editor/UIElements/NodeView.cs
@@ -8,6 +8,11 @@ namespace TreeFlow.Editor.UIElements
     public sealed class NodeView : Node
     {
         /// <summary>
+        /// Current node displayed
+        /// </summary>
+        public GraphNode Node { get; private set; }
+        
+        /// <summary>
         /// Assigns the node to display to the given <see cref="GraphNode"/>
         /// </summary>
         /// <param name="node"></param>
@@ -16,6 +21,8 @@ namespace TreeFlow.Editor.UIElements
             var rect = GetPosition();
             rect.position = node.Position;
             SetPosition(rect);
+            
+            Node = node;
         }
     }
 }
--- Editor/UIElements/TreeGraphView.cs
+++ Editor/UIElements/TreeGraphView.cs
@@ -21,14 +21,6 @@ namespace TreeFlow.Editor.UIElements
             this.AddManipulator(new RectangleSelector());
         }
 
-        public override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
-        {
-            evt.menu.AppendAction("Test", action =>
-            {
-                CreateNode(action.eventInfo.mousePosition);
-            });
-        }
-
         #region UI
 
         public new class UxmlFactory : UxmlFactory<TreeGraphView, UxmlTraits> { }
@@ -45,6 +37,56 @@ namespace TreeFlow.Editor.UIElements
 
         #endregion
 
+        #region Input
+
+        /// <inheritdoc/>
+        public override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
+        {
+            evt.menu.AppendAction("Test", action =>
+            {
+                CreateNode(action.eventInfo.mousePosition);
+            });
+        }
+        
+        private GraphViewChange OnGraphViewChanged(GraphViewChange graphViewChange)
+        {
+            // Nodes moved
+            if (graphViewChange.movedElements != null)
+            {
+                var newPositions = new Dictionary<string, Vector2>();
+
+                foreach (var element in graphViewChange.movedElements)
+                {
+                    if (element is not NodeView nodeView)
+                        continue;
+
+                    newPositions.TryAdd(nodeView.Node.GUID, nodeView.GetPosition().position);
+                }
+                
+                MoveNodes(newPositions);
+            }
+
+            // Nodes removed
+            if (graphViewChange.elementsToRemove != null)
+            {
+                var nodesToRemove = new List<GraphNode>();
+
+                foreach (var element in graphViewChange.elementsToRemove)
+                {
+                    if (element is not NodeView nodeView)
+                        continue;
+                    
+                    nodesToRemove.Add(nodeView.Node);
+                }
+                
+                RemoveNodes(nodesToRemove);
+            }
+            
+            return graphViewChange;
+        }
+
+        #endregion
+
         #region Tree
 
         private SerializedObject serializedTree;
@@ -57,14 +99,26 @@ namespace TreeFlow.Editor.UIElements
         /// </summary>
         public void AssignTree(SerializedObject obj)
         {
+            graphViewChanged -= OnGraphViewChanged;
+            
+            foreach (var (_, nodeView) in nodeViewsByGuid)
+                RemoveElement(nodeView);
+
+            nodeViewsByGuid.Clear();
+            
             serializedTree = obj;
             treeAsset = (BehaviorTreeAsset)serializedTree.targetObject;
+
+            foreach (var node in treeAsset.Nodes)
+                AddNodeToGraph(node);
+            
+            graphViewChanged += OnGraphViewChanged;
         }
 
         /// <summary>
         /// Creates a brand new <see cref="GraphNode"/> from the given information
         /// </summary>
-        public void CreateNode(Vector2 position)
+        private void CreateNode(Vector2 position)
         {
             var newNode = new GraphNode
             {
@@ -77,6 +131,42 @@ namespace TreeFlow.Editor.UIElements
             serializedTree.Update();
             
             AddNodeToGraph(newNode);
+        }
+
+        /// <summary>
+        /// Removes the given nodes from the graph
+        /// </summary>
+        private void RemoveNodes(List<GraphNode> graphNodes)
+        {
+            if (graphNodes.Count == 0)
+                return;
+            
+            Undo.RecordObject(serializedTree.targetObject, "Removed Nodes");
+            foreach (var graphNode in graphNodes)
+            {
+                treeAsset.Nodes.Remove(graphNode);
+                nodeViewsByGuid.Remove(graphNode.GUID);
+            }
+            serializedTree.Update();
+        }
+
+        /// <summary>
+        /// Moves the given nodes at the given positions
+        /// </summary>
+        private void MoveNodes(Dictionary<string, Vector2> positions)
+        {
+            if (positions.Count == 0)
+                return;
+            
+            Undo.RecordObject(serializedTree.targetObject, "Moved Nodes");
+            foreach (var (guid, position) in positions)
+            {
+                if (!nodeViewsByGuid.TryGetValue(guid, out var nodeView))
+                    continue;
+
+                nodeView.Node.Position = position;
+            }
+            serializedTree.Update();
         }
 
         /// <summary>
