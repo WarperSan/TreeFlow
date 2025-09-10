efa32905a016346b508c407873bf9951542b0722
WarperSan <leumas.ecole@gmail.com>
2025-09-10 12:46:00 -0400

    Simplified CompositeNodeAsset.cs

--- Editor/Nodes/Core/CompositeNodeAsset.cs
+++ Editor/Nodes/Core/CompositeNodeAsset.cs
@@ -15,34 +15,9 @@ namespace TreeFlow.Editor.Nodes.Core
         [SerializeField] private List<string> m_children = new();
         
         /// <inheritdoc/>
-        public void Link(NodeAsset child)
-        {
-            m_children.Add(child.GUID);
-            uniqueChildren.Add(child.GUID);
-        }
+        public void Link(NodeAsset child) => m_children.Add(child.GUID);
 
         /// <inheritdoc/>
-        public void Unlink(NodeAsset child)
-        {
-            m_children.Remove(child.GUID);
-            uniqueChildren.Remove(child.GUID);
-        }
-
-        #region Utils
-
-        [NonSerialized] private readonly HashSet<string> uniqueChildren = new();
-
-        /// <inheritdoc/>
-        public override void Compute(BehaviorTreeAsset tree)
-        {
-            base.Compute(tree);
-            
-            uniqueChildren.Clear();
-            
-            foreach (var child in m_children)
-                uniqueChildren.Add(child);
-        }
-
-        #endregion
+        public void Unlink(NodeAsset child) => m_children.Remove(child.GUID);
     }
 }
